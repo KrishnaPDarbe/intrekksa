7a37eb5bf450a16d4e8176d6e12ec7520c3e43d5
D krishna Prasad <d.krishna.prasad@sap.com>
2023-02-06 09:08:00 +0530

    first design commit

---
+++ src/app/components/home-page/home-page.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'intrekksarl-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
+})
+export class HomePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  test(){
+    alert('hello')
+  }
+}
