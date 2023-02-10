05ef7b149aad18f1c3a1c6e05bb0b7e7c1bc8c42
D krishna Prasad <d.krishna.prasad@sap.com>
2023-02-10 20:57:09 +0530

    deployment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     component:HomePageComponent
   },
   {
+    path:'intrekksa/travelAgency',
+    component:HomePageComponent
+  },
+  {
     path: 'travelAgency',
     component: TravelAgencyComponent,
   },
